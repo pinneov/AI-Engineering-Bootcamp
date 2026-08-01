af1fa1b1b3fd98a66ae0fd97273f3fe4dfd9521e
Vincent Pinneo <vpinneo@cox.net>
2026-08-01 14:27:31 -0700

    Improve Day 2 profile output formatting

--- Month-01-Python/Day-02/profile.py
+++ Month-01-Python/Day-02/profile.py
@@ -4,7 +4,8 @@ fav_programming_lang = input("What is your favorite programming language? ")
 years_experience = int(input("How many years of experience do you have? "))
 next_year_exp = years_experience + 1
 
-print("Developer Profile")
+print("\nDeveloper Profile")
+print("-" * 20)
 print(f"Name: {name}")
 print(f"Age: {age}")
 print(f"Favorite Language: {fav_programming_lang}")
